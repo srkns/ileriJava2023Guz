b8c81d9f66a7e3ea6154ad3bdb0699dbb1c4d275
srkns <serkanisleyen@gmail.com>
2023-10-18 19:59:16 +0300

    Ders2
    Yorumlar, değişken tipleri, operatörler

---
+++ Ders2/src/OrnekSinif.java
@@ -0,0 +1,55 @@
+public class OrnekSinif { // pascal case
+
+    /*
+    *   Aşağıda programın çıktılarını yazdırdım
+    *   çok satırlı
+    *
+    */
+    public static void main(String[] args) {
+
+        final int pi;
+        int sayi = 5;
+        ////
+        sayi = sayi * 7;
+        pi = 35;
+        //pi = 3;
+        System.out.print("Hello World!");//merhaba
+        System.out.println("I am learning Java.");//eğitimin amacı
+        System.out.println("It is awesome!");
+
+        System.out.println(3 + 3);
+        System.out.println("358" + 4);
+        System.out.println(50000);
+        //pi = 5;
+        String ad = "Can";
+        System.out.println(ad);
+        ad = "Medipol";
+        System.out.println(ad);
+
+        int myNum;
+        myNum = 15;
+        System.out.println(myNum);
+
+        String name = "John";
+        System.out.println("Hello " +
+
+
+
+                name);
+
+        int x = 5;
+        int y = 6;
+        System.out.println(x + y);
+
+        String kisininIsmi = "MEdİpOl"; // camel case
+
+        System.out.println(kisininIsmi.toUpperCase());
+
+        int sayimiz = 6;
+        System.out.println(sayimiz);
+        sayimiz++; // sayimiz = sayimiz + 1; // sayimiz+=1;
+        System.out.println(sayimiz);
+
+    }
+
+}
